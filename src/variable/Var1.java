1b9284aa53375e6af3acbd0779c71f976de0f91b
RamzThunder <139237219+RamzThunder@users.noreply.github.com>
2025-06-11 22:43:12 +0900

    Study

--- src/variable/Var1.java
+++ src/variable/Var1.java
@@ -3,7 +3,7 @@ package variable;
 public class Var1{
     public static void main(String[] args) {
         int a = 10;
-        int b = 30;
+        int b = 40;
 
         int max = (a > b) ? a : b;
     }
