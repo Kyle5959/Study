4b83361ded5ceb100dc9490093466eda032ae00a
RamzThunder <139237219+RamzThunder@users.noreply.github.com>
2025-06-16 22:01:36 +0900

    Study

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -1,29 +1,16 @@
-import java.util.Scanner;
-
 public class HelloJava {
     public static void main(String[] args) {
+        int num1 = 5;
+        System.out.println(num1);
+        multiple(num1);
+        System.out.println(num1);
 
-        Scanner scanner = new Scanner(System.in);
-        Long sum4 = 0L;
+        int num2 = num1;
 
-        while (true){
-            System.out.print("첫번째 숫자 : ");
-            Long a = scanner.nextLong();
-            System.out.print("두번째 숫자 : ");
-            Long b = scanner.nextLong();
-            Long sum = a + b;
-
-            System.out.println("합한 숫자 : " + sum);
-
-            sum4 += sum;
-
-            if (a == 0 || b == 0) {
-                System.out.println("프로그램을 종료합니다.");
-                System.out.println("입력한 모든 숫자의 합 : " + sum4);
-                break;
-
-            }
-
-        }
     }
+    public static void multiple (int num2) {
+
+        num2 = num2 * 2;
+    }
+
 }
